801243d1d15532ce5aee5c0d5a1c0d67732aa7f8
Anna <annamariej1015@gmail.com>
2021-05-11 16:59:44 -0700

    feature: data set type for Drinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return(
-        <div>Navbar</div>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <Link to='/' className='navbar-brand'>
+                Drinksy
+            </Link>
+
+            <div className='collapse navbar-collapse'>
+                <div className='navbar-nav'>
+                    <Link className='nav-link' to='/'>
+                        Home
+                    </Link>
+                </div>
+            </div>
+        </nav>
     )
 };
 
