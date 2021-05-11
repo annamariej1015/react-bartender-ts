75842671cdff18075b1f00f87ce3071f8af2c5b2
Anna <annamariej1015@gmail.com>
2021-05-11 14:43:03 -0700

    AppRoutes file add with base structure

---
+++ src/router/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { Switch, Route } from 'react-router-dom';
+// page components
+import HomePage from '../pages/HomePage';
+import SingleDrinkPage from '../pages/SingleDrink';
+// Components
+import Navbar from '../components/Navbar';
+
+const AppRoutes = () => {
+    return (
+    <div>
+        <Navbar />
+        <div className='container-fluid'>
+        <Switch>
+            <Route path='/' exact component={HomePage} />
+            <Route path='/' component={SingleDrinkPage}/>
+        </Switch>
+        </div>
+    </div>
+    );
+};
+
+export default AppRoutes;
