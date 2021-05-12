910898c25729b10d97c973b4108f8bfe91e13a51
pravallika <pravallikaammineni@github.com>
2021-05-11 18:18:39 -0700

    concluding merges in APP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './router/AppRoutes';
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 79060bab0f36b4637d87d430fff2b69314f4a81e
 const App = () => {
   return (
     <Router>
       <AppRoutes />
     </Router>
-<<<<<<< HEAD
-=======
-    
->>>>>>> 79060bab0f36b4637d87d430fff2b69314f4a81e
+
   );
 }
 
